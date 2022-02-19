33c1ae000bec1e4acd259a29a416eff5b20f5701
RaviHamidov <ravihamidov42@gmail.com>
2022-02-19 20:13:39 +0400

    GitHub Organize

---
+++ 01_Variables/JavaScript/01_Variables.js
@@ -0,0 +1,10 @@
+// Only when declared, When no value is given ( Type > undefined )
+let variable
+console.log(variable)
+
+// The type is assigned according to the given value
+var age = 18
+console.log(age)
+
+// When the value of a variable given in the previous line is reused in the new line, it will retrieve the information in the new line.
+

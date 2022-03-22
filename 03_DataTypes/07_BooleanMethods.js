51e45b2c22e5c1b3d829d2df9328660d4da01a85
RaviHamidov <ravihamidov42@gmail.com>
2022-03-22 08:43:44 +0400

    JavaScript > DataTypes > BooleanMethods

---
+++ 03_DataTypes/07_BooleanMethods.js
@@ -0,0 +1,8 @@
+// Boolean Methods
+/// Primitive or Boolean object includes following methods.
+
+// -> toString()
+var result = (1 > 2); result.toString(); // returns "false"
+
+// -> valueOf()
+var result = (1 > 2); result.valueOf(); // returns false
